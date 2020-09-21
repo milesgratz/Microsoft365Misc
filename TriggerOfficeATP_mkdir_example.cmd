77c6f0b9b1e9e04e5d4d71d38d06418c49cf4daa
Miles Gratz <milesgratz@gmail.com>
2020-09-21 07:12:08 -0500

    fix paths

--- TriggerOfficeATP_mkdir_example.cmd
+++ TriggerOfficeATP_mkdir_example.cmd
@@ -1,4 +1,4 @@
 @echo off
-mkdir C:\TriggerOfficeATP
-echo "You triggered Office ATP using a macro enabled Word doc ... don't forget to clean this up..." > "C:\TriggerOfficeATP\readme.txt"
-echo "https://github.com/milesgratz/Microsoft365Misc/blob/master/TriggerOfficeATP.md" >> "C:\TriggerOfficeATP\readme.txt"
+mkdir "%USERPROFILE%\TriggerOfficeATP"
+echo "You triggered Office ATP using a macro enabled Word doc ... don't forget to clean this up..." > "%USERPROFILE%\TriggerOfficeATP\readme.txt"
+echo "https://github.com/milesgratz/Microsoft365Misc/blob/master/TriggerOfficeATP.md" >> "%USERPROFILE%\TriggerOfficeATP\readme.txt"
